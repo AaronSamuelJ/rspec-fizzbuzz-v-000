3ed040b57c80b003c59ecbfc37bb332517e5ec7c
Aaron Samuel <aasamuelj@gmail.com>
2017-09-22 08:23:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,12 @@
 require 'pry'
 def fizzbuzz(num)
-  binding.pry
+  if num % 5 == 0 && num % 3 == 0
+    "fizzbuzz"
+  elsif num % 5 == 0
+    "fizz"
+  elsif num % 3 == 0
+    "buzz"
+  else
+    nil
+  end
 end
